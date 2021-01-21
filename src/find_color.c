f553e4e45512e55836e603772a23eb779ef84bbc
vkuikka <villekuikka1@gmail.com>
2021-01-21 19:39:15 +0200

    fixed indentation

--- src/find_color.c
+++ src/find_color.c
@@ -6,37 +6,37 @@
 /*   By: vkuikka <vkuikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/21 17:32:09 by vkuikka           #+#    #+#             */
-/*   Updated: 2021/01/21 19:11:29 by vkuikka          ###   ########.fr       */
+/*   Updated: 2021/01/21 19:38:58 by vkuikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "doom-nukem.h"
 
 void	line_intersection(float res[2], float a[2], float b[2], float c[2], float d[2]) 
-{ 
-    // Line AB represented as a1x + b1y = c1 
-    double a1 = b[1] - a[1]; 
-    double b1 = a[0] - b[0]; 
-    double c1 = a1 * a[0] + b1 * a[1]; 
-  
-    // Line CD represented as a2x + b2y = c2 
-    double a2 = d[1] - c[1]; 
-    double b2 = c[0] - d[0]; 
-    double c2 = a2 * c[0] + b2 * c[1]; 
-  
-    double determinant = a1*b2 - a2*b1; 
-  
-    // if (determinant == 0) 
-    // { 
-        // The lines are parallel. This is simplified 
-        // by returning a pair of FLT_MAX 
-        // return make_pair(FLT_MAX, FLT_MAX); 
-    // } 
-    // else
-    { 
-        res[0] = (b2*c1 - b1*c2)/determinant; 
-        res[1] = (a1*c2 - a2*c1)/determinant; 
-    } 
+{
+	// Line AB represented as a1x + b1y = c1
+	double a1 = b[1] - a[1];
+	double b1 = a[0] - b[0];
+	double c1 = a1 * a[0] + b1 * a[1];
+
+	// Line CD represented as a2x + b2y = c2
+	double a2 = d[1] - c[1];
+	double b2 = c[0] - d[0];
+	double c2 = a2 * c[0] + b2 * c[1];
+
+	double determinant = a1*b2 - a2*b1;
+
+	// if (determinant == 0)
+	// {
+		// The lines are parallel. This is simplified
+		// by returning a pair of FLT_MAX
+		// return make_pair(FLT_MAX, FLT_MAX);
+	// }
+	// els
+	{
+		res[0] = (b2*c1 - b1*c2)/determinant;
+		res[1] = (a1*c2 - a2*c1)/determinant;
+	}
 }
 
 void	rot2d(float res[2], float v[2], float rad)
