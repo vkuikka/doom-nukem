88430b635c145dc4576b6ebf11669638799fc8ae
rpehkone <pehkonenroni@gmail.com>
2021-05-19 21:29:02 +0300

    Added files for hud and main menu

---
+++ src/main_menu.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_menu.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpehkone <rpehkone@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/19 18:51:47 by rpehkone          #+#    #+#             */
+/*   Updated: 2021/05/19 18:51:47 by rpehkone         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "doom-nukem.h"
+
+void	main_menu_move_background(t_level *level)
+{
+	//lerp pos1 pos2
+}
+
+void	main_menu(t_level *level)
+{
+	//open level
+	//edit level
+	//new level
+	//exit
+}
