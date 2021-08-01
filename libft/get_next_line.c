39e50cfabb6554aa3fdb3a517f09b897f85e7b32
vkuikka <villekuikka1@gmail.com>
2021-08-01 19:58:53 +0300

    cleaned get_next_line

--- libft/get_next_line.c
+++ libft/get_next_line.c
@@ -6,35 +6,13 @@
 /*   By: vkuikka <vkuikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/03 13:01:40 by vkuikka           #+#    #+#             */
-/*   Updated: 2019/12/13 17:55:10 by vkuikka          ###   ########.fr       */
+/*   Updated: 2021/08/01 19:45:30 by vkuikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char		*ft_free_line(char *input)
-{
-	char	*cpy;
-	int		i;
-
-	i = 0;
-	while (input[i] && input[i] != '\n')
-		i++;
-	if (input[i] == '\n')
-		i++;
-	cpy = ft_strdup(&input[i]);
-	i = 0;
-	while (cpy[i])
-		i++;
-	if (input && *input)
-	{
-		free(input);
-		input = NULL;
-	}
-	return (cpy);
-}
-
-static char		*ft_strdup_nl(char *src)
+static char	*ft_strdup_nl(char *src)
 {
 	int		i;
 	int		count;
@@ -44,7 +22,8 @@ static char		*ft_strdup_nl(char *src)
 	i = 0;
 	while (src && src[i] && src[i] != '\n')
 		i++;
-	if (!(str = (char*)malloc(sizeof(str) * (i + 1))))
+	str = (char *)malloc(sizeof(str) * (i + 1));
+	if (!str)
 		return (NULL);
 	while (count < i && src[count] && src[count] != '\n')
 	{
@@ -55,7 +34,7 @@ static char		*ft_strdup_nl(char *src)
 	return (str);
 }
 
-static char		*ft_strjoin_free(char *s1, char const *s2, int stop_nl)
+static char	*ft_strjoin_free(char *s1, char const *s2, int stop_nl)
 {
 	char	*str;
 	int		len_1;
@@ -63,12 +42,12 @@ static char		*ft_strjoin_free(char *s1, char const *s2, int stop_nl)
 
 	len_1 = 0;
 	len_2 = 0;
-	if (s1 && *s1)
-		while (s1[len_1] && (s1[len_1] != '\n' || !stop_nl))
-			len_1++;
+	while (s1 && *s1 && s1[len_1] && (s1[len_1] != '\n' || !stop_nl))
+		len_1++;
 	while (s2[len_2] && (s2[len_2] != '\n' || !stop_nl))
 		len_2++;
-	if (!(str = (char *)malloc(len_1 + len_2 + 1)))
+	str = (char *)malloc(len_1 + len_2 + 1);
+	if (!str)
 		return (NULL);
 	len_1 = -1;
 	if (s1 && !*s1)
@@ -84,7 +63,7 @@ static char		*ft_strjoin_free(char *s1, char const *s2, int stop_nl)
 	return (str);
 }
 
-int				get_next_line(const int fd, char **line)
+int	get_next_line(const int fd, char **line)
 {
 	static char		*str[MAX_FD];
 	char			buff[BUFF_SIZE + 1];
@@ -93,22 +72,19 @@ int				get_next_line(const int fd, char **line)
 
 	i = 0;
 	read_ret = 1;
-	if (!line || fd < 0 || (read(fd, buff, 0) < 0) ||
-	(!(str[fd]) && !(str[fd] = ft_strnew(BUFF_SIZE))))
+	if (!first_checks(str, line, fd, buff))
 		return (-1);
 	while (str[fd][i] && str[fd][i] != '\n')
 		i++;
-	while (str[fd][i] != '\n' && read_ret > 0 && !(i = 0))
+	while (str[fd][i] != '\n' && read_ret > 0)
 	{
 		read_ret = read(fd, buff, BUFF_SIZE);
 		buff[read_ret] = '\0';
 		str[fd] = ft_strjoin_free(str[fd], buff, 0);
+		i = 0;
 		while (str[fd][i] && str[fd][i] != '\n')
 			i++;
 	}
-	if (!(*line = ft_strdup_nl(str[fd])))
-		read_ret = -1;
-	if (*str[fd] && (read_ret = 1))
-		str[fd] = ft_free_line(str[fd]);
-	return (read_ret);
+	*line = ft_strdup_nl(str[fd]);
+	return (return_value(read_ret, str, fd, line));
 }
