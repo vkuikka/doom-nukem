e1bf545a9002b012fef71f1f9cf29147bc4586bb
rpehkone <pehkonenroni@gmail.com>
2021-08-10 15:08:26 +0300

    audio.c

--- src/audio.c
+++ src/audio.c
@@ -3,62 +3,57 @@
 /*                                                        :::      ::::::::   */
 /*   audio.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alcohen <alcohen@student.hive.fi>          +#+  +:+       +#+        */
+/*   By: rpehkone <rpehkone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/09 21:56:12 by alcohen           #+#    #+#             */
-/*   Updated: 2021/06/09 23:23:50 by alcohen          ###   ########.fr       */
+/*   Updated: 2021/08/10 15:05:47 by rpehkone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "doom-nukem.h"
 
-static void     handle_jump_sound(t_level *level)
+static void	handle_jump_sound(t_level *level)
 {
-    t_audio *audio;
+	const Uint8	*keys;
+	t_audio		*audio;
 
-    audio = &level->audio;
-    if (!audio->played_jump_sound && is_player_in_air(level, PLAYER_HEIGHT))
-    {
-        Mix_PlayChannel(AUDIO_JUMP_CHANNEL, level->audio.jump, 0);
-        audio->played_jump_sound = TRUE;
-    }
-    if (!is_player_in_air(level, PLAYER_HEIGHT))
-    {
-        audio->played_jump_sound = FALSE;
-    }
+	keys = SDL_GetKeyboardState(NULL);
+	if (!keys[SDL_SCANCODE_SPACE])
+		return ;
+	audio = &level->audio;
+	if (!audio->played_jump_sound && is_player_in_air(level, PLAYER_HEIGHT))
+	{
+		Mix_PlayChannel(AUDIO_JUMP_CHANNEL, level->audio.jump, 0);
+		audio->played_jump_sound = TRUE;
+	}
+	if (!is_player_in_air(level, PLAYER_HEIGHT))
+	{
+		audio->played_jump_sound = FALSE;
+	}
 }
 
-void            handle_audio(t_level *level, t_game_state *game_state)
+void	handle_audio(t_level *level, t_game_state *game_state)
 {
-    t_audio *audio;
-    const Uint8		*keys;
-    
-    audio = &level->audio;
-    keys = SDL_GetKeyboardState(NULL);
-    if (level->ui.state.m1_click && level->ui.state.mouse_capture && *game_state != GAME_STATE_DEAD)
+	if (level->ui.state.m1_click && level->ui.state.mouse_capture
+		&& *game_state != GAME_STATE_DEAD)
 	{
 		if (level->player_ammo)
-		{
 			Mix_PlayChannel(AUDIO_GUNSHOT_CHANNEL, level->audio.gunshot, 0);
-		}
 		else
-		{
 			Mix_PlayChannel(AUDIO_RELOAD_CHANNEL, level->audio.reload, 0);
-		}
 	}
 	else if (level->player_health <= 0)
 	{
 		Mix_HaltMusic();
 		Mix_PlayChannel(AUDIO_DEATH_CHANNEL, level->audio.death, 0);
 	}
-    if (level->reload_start_time && !audio->played_reload_sound)
-    {
-        if (!Mix_Playing(AUDIO_RELOAD_CHANNEL))
-            Mix_PlayChannel(AUDIO_RELOAD_CHANNEL, level->audio.reload, 0);
-        audio->played_reload_sound = TRUE;
-    }
-    if (level->reload_start_time == 0)
-        audio->played_reload_sound = FALSE;
-    if (keys[SDL_SCANCODE_SPACE])
-        handle_jump_sound(level);
+	if (level->reload_start_time && !level->audio.played_reload_sound)
+	{
+		if (!Mix_Playing(AUDIO_RELOAD_CHANNEL))
+			Mix_PlayChannel(AUDIO_RELOAD_CHANNEL, level->audio.reload, 0);
+		level->audio.played_reload_sound = TRUE;
+	}
+	if (level->reload_start_time == 0)
+		level->audio.played_reload_sound = FALSE;
+	handle_jump_sound(level);
 }
