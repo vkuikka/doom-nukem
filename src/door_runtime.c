067262ad0fceffa27f3c680354c61ef6fbd21185
rpehkone <pehkonenroni@gmail.com>
2021-08-11 19:16:58 +0300

    door_runtime.c

--- src/door_runtime.c
+++ src/door_runtime.c
@@ -6,7 +6,7 @@
 /*   By: rpehkone <rpehkone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/10 19:12:42 by rpehkone          #+#    #+#             */
-/*   Updated: 2021/08/10 19:43:16 by rpehkone         ###   ########.fr       */
+/*   Updated: 2021/08/11 19:14:53 by rpehkone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,14 @@ void	door_activate(t_level *level)
 	t_door	*door;
 	t_vec3	avg;
 	int		amount;
+	int		i;
+	int		j;
+	int		k;
 
 	nearest_index = -1;
 	nearest_len = FLT_MAX;
-	for (int i = 0; i < level->doors.door_amount; i++)
+	i = -1;
+	while (++i < level->doors.door_amount)
 	{
 		ft_memset(&avg, 0, sizeof(t_vec3));
 		door = &level->doors.door[i];
@@ -32,11 +36,14 @@ void	door_activate(t_level *level)
 		else
 		{
 			amount = 0;
-			for (int j = 0; j < door->indice_amount; j++)
+			j = -1;
+			while (++j < door->indice_amount)
 			{
-				for (int k = 0; k < 3 + door->isquad[j]; k++)
+				k = -1;
+				while (++k < 3 + door->isquad[j])
 				{
-					vec_add(&avg, avg, level->all.tris[door->indices[j]].verts[k].pos);
+					vec_add(&avg, avg,
+						level->all.tris[door->indices[j]].verts[k].pos);
 					amount++;
 				}
 			}
@@ -54,13 +61,13 @@ void	door_activate(t_level *level)
 	{
 		Mix_PlayChannel(AUDIO_DOOR_CHANNEL, level->audio.door, 0);
 		door = &level->doors.door[nearest_index];
-		door->transition_direction = door->transition_direction ? 0 : 1;
+		door->transition_direction = door->transition_direction == 0;
 		if ((SDL_GetTicks() - door->transition_start_time)
 			 / (1000 * door->transition_time) < 1)
 		{
 			door->transition_start_time = SDL_GetTicks()
 				- ((door->transition_time * 1000)
-				- (SDL_GetTicks() - door->transition_start_time));
+					- (SDL_GetTicks() - door->transition_start_time));
 		}
 		else
 			door->transition_start_time = SDL_GetTicks();
@@ -71,31 +78,43 @@ void	door_animate(t_level *level)
 {
 	t_door	*door;
 	float	time;
+	int		a;
+	int		i;
+	int		k;
 
-	for (int a = 0; a < level->doors.door_amount; a++)
+	a = -1;
+	while (++a < level->doors.door_amount)
 	{
 		door = &level->doors.door[a];
 		if (door->transition_start_time)
 		{
 			time = (SDL_GetTicks() - door->transition_start_time)
 				/ (1000 * door->transition_time);
-			for (int i = 0; i < door->indice_amount; i++)
+			i = -1;
+			while (++i < door->indice_amount)
 			{
-				for (int k = 0; k < 3 + door->isquad[i]; k++)
+				k = -1;
+				while (++k < 3 + door->isquad[i])
 				{
 					if (time < 1)
 					{
 						if (door->transition_direction)
-							level->all.tris[door->indices[i]].verts[k].pos = vec_interpolate(door->pos2[i][k], door->pos1[i][k], time);
+							level->all.tris[door->indices[i]].verts[k].pos
+								= vec_interpolate(door->pos2[i][k],
+									door->pos1[i][k], time);
 						else
-							level->all.tris[door->indices[i]].verts[k].pos = vec_interpolate(door->pos1[i][k], door->pos2[i][k], time);
+							level->all.tris[door->indices[i]].verts[k].pos
+								= vec_interpolate(door->pos1[i][k],
+									door->pos2[i][k], time);
 					}
 					else
 					{
 						if (door->transition_direction)
-							level->all.tris[door->indices[i]].verts[k].pos = door->pos1[i][k];
+							level->all.tris[door->indices[i]]
+								.verts[k].pos = door->pos1[i][k];
 						else
-							level->all.tris[door->indices[i]].verts[k].pos = door->pos2[i][k];
+							level->all.tris[door->indices[i]]
+								.verts[k].pos = door->pos2[i][k];
 						door->transition_start_time = 0;
 					}
 				}
