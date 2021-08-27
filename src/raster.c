0a8429eab5ca52b580dd21e9233f03388b929326
vkuikka <villekuikka1@gmail.com>
2021-08-27 15:40:48 +0300

    implemented normal maps in rasterization and added some brightness to demo level so loaded obj can be seen

--- src/raster.c
+++ src/raster.c
@@ -6,11 +6,28 @@
 /*   By: vkuikka <vkuikka@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 23:51:41 by vkuikka           #+#    #+#             */
-/*   Updated: 2021/08/16 00:02:35 by vkuikka          ###   ########.fr       */
+/*   Updated: 2021/08/27 15:37:09by vkuikka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "doom_nukem.h"
+
+t_ray	ray_set(t_camera *cam, float fov, t_vec2 xy)
+{
+	t_ray	res;
+	float	xm;
+	float	ym;
+
+	xm = fov * ((float)RES_X / RES_Y);
+	ym = fov;
+	xm = xm / RES_X * (int)xy.x - xm / 2;
+	ym = ym / RES_Y * (int)xy.y - ym / 2;
+	res.dir.x = cam->front.x + cam->up.x * ym + cam->side.x * xm;
+	res.dir.y = cam->front.y + cam->up.y * ym + cam->side.y * xm;
+	res.dir.z = cam->front.z + cam->up.z * ym + cam->side.z * xm;
+	res.pos = cam->pos;
+	return (res);
+}
 
 void			ss_minmax(t_vec2 *min, t_vec2 *max, t_ss_tri tri, int resx, int resy)
 {
@@ -37,7 +54,7 @@ void			ss_minmax(t_vec2 *min, t_vec2 *max, t_ss_tri tri, int resx, int resy)
 	max->y = fmin(resy, max->y);
 }
 
-t_vec3			ss_to_uv(t_vec2 fp, t_ss_tri *tri, t_bmp *txtr)
+t_vec3			ss_to_uv(t_vec2 fp, t_ss_tri *tri, t_bmp *txtr, float *dist)
 {
 	t_vec2	v0;
 	t_vec2	v1;
@@ -60,34 +77,34 @@ t_vec3			ss_to_uv(t_vec2 fp, t_ss_tri *tri, t_bmp *txtr)
 	uvw.y *= 1 / tri->verts[1].pos.z;
 	uvw.x *= 1 / tri->verts[2].pos.z;
 
-	// float dist = (uvw.z * tri->verts[0].pos.z +
-	// 		uvw.y * tri->verts[1].pos.z +
-	// 		uvw.x * tri->verts[2].pos.z) / (float)(uvw.x + uvw.y + uvw.z);
+	*dist = (uvw.z * tri->verts[0].pos.z +
+			uvw.y * tri->verts[1].pos.z +
+			uvw.x * tri->verts[2].pos.z) / (float)(uvw.x + uvw.y + uvw.z);
 
 	normalize_data(&uvw);
 	return (uvw);
 }
 
-unsigned int			raster_face_color(float u, float v, t_ss_tri t, t_bmp *texture)
+t_ivec2	raster_texture_coord(float u, float v, t_ss_tri t, t_bmp *texture)
 {
-	int		x;
-	int 	y;
+	t_ivec2	res;
 	float	w;
 
 	w = 1 - u - v;
-	x =	((t.verts[0].uv.x * texture->width * w +
+	res.x =	((t.verts[0].uv.x * texture->width * w +
 			t.verts[1].uv.x * texture->width * v +
 			t.verts[2].uv.x * texture->width * u) / (float)(u + v + w));
-	y =	((t.verts[0].uv.y * texture->height * w +
+	res.y =	((t.verts[0].uv.y * texture->height * w +
 			t.verts[1].uv.y * texture->height * v +
 			t.verts[2].uv.y * texture->height * u) / (float)(u + v + w));
-	wrap_coords(&x, &y, texture->width, texture->height);
-	return(texture->image[x + (y * texture->width)]);
+	wrap_coords(&res.x, &res.y, texture->width, texture->height);
+	return(res);
 }
 
 int				raster(void *data_pointer)
 {
 	t_rthread		*t = data_pointer;
+	t_cast_result	res;
 	t_vec2			screen;
 	t_vec3			uv;
 	t_vec2			min;
@@ -100,6 +117,7 @@ int				raster(void *data_pointer)
 	int resy = RES_Y / t->level->ui.raycast_quality;
 
 	i = 0;
+	res.raytracing = FALSE;
 	all = &t->level->ss_tris[0];
 	while (i < t->level->ss_tri_amount)
 	{
@@ -127,9 +145,22 @@ int				raster(void *data_pointer)
 				if (!t->window->frame_buffer[(int)screen.x + ((int)screen.y * RES_X)]
 				&& point_in_tri(screen, a, b, c))
 				{
-					uv = ss_to_uv(screen, tri, &t->level->texture);
-					t->window->frame_buffer[(int)screen.x + ((int)screen.y * RES_X)]
-					= raster_face_color(uv.x, uv.y, *tri, &t->level->texture);
+					float dist;
+					uv = ss_to_uv(screen, tri, &t->level->texture, &dist);
+					t_ivec2	txtr = raster_texture_coord(uv.x, uv.y, *tri, &t->level->texture);
+
+					res.ray = ray_set(&t->level->cam, t->level->ui.fov, screen);
+					vec_mult(&res.ray.dir, dist);
+					vec_add(&res.ray.pos, res.ray.pos, res.ray.dir);
+
+					res.color = t->level->texture.image[txtr.x + txtr.y * t->level->texture.width];
+					res.normal = get_normal(t->level->normal_map.image[txtr.x + txtr.y * t->level->normal_map.width]);
+					vec_normalize(&res.normal);
+
+					t_color light;
+					light = lights(t->level, &res, res.normal);
+					res.color = brightness(res.color >> 8, light) + (res.color << 24 >> 24);
+					t->window->frame_buffer[(int)screen.x + ((int)screen.y * RES_X)] = res.color;
 				}
 				screen.y++;
 			}
