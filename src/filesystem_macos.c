558c09d240104699ccd17e0a7232cbd11881b9db
rpehkone <pehkonenroni@gmail.com>
2021-11-24 00:47:40 +0200

    collapse relative path, set working directory to executable folder

--- src/filesystem_macos.c
+++ src/filesystem_macos.c
@@ -6,7 +6,7 @@
 /*   By: rpehkone <rpehkone@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/21 04:01:29 by rpehkone          #+#    #+#             */
-/*   Updated: 2021/08/21 21:00:21 by rpehkone         ###   ########.fr       */
+/*   Updated: 2021/11/24 00:45:14 by rpehkone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,16 @@ char	*get_current_directory(void)
 {
 	uint32_t	path_max_size;
 	char		*str;
+	char		*abs;
 
 	str = (char *)malloc(sizeof(char) * PATH_MAX);
 	if (!str)
 		ft_error("memory allocation failed\n");
 	path_max_size = PATH_MAX - 2;
 	_NSGetExecutablePath(str, &path_max_size);
-	path_up_dir(str);
+	abs = realpath(str, NULL);
+	free(str);
+	str = abs;
 	path_up_dir(str);
 	return (str);
 }
